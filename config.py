04fea66cd0e0379b213bca30c79bb2d117b42c80
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-02-03 16:39:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ import requests
 API_ID = int(environ.get("API_ID", "10811400"))
 API_HASH = environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
 BOT_TOKEN = environ.get("BOT_TOKEN", "6371414039:AAFUvQJR7UcyVa2ox-nZv_YDGhNmgdYPmTE")
+REMIKEY = environ.get("REMIKEY", "2Q3MHwUTP5f_HK8Kd6nTRK-XPTQOft3pxAkBzqh9QBN-6Tb4")
 BOT_USERNAME = environ.get("BOT_USERNAME", "DALLE24BOT")
 OWNER_ID = environ.get("OWNER_ID", "6469754522")
 FORCESUB_CHANNEL = environ.get("FORCESUB_CHANNEL", "-1002131605976")
