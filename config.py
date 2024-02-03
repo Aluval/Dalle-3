591b3e24fdf3246fae4dbbefd8c352ab666751a3
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-02-03 09:03:34 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+from os import environ
+
+import requests
+
+API_ID = int(environ.get("API_ID", 0))
+API_HASH = environ.get("API_HASH", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_USERNAME = environ.get("BOT_USERNAME", "")
+owner = environ.get("OWNER_ID", "")
+FORCESUB_CHANNEL = environ.get("FORCESUB_CHANNEL", "")
+FORCESUB_CHANNEL_UNAME = environ.get("FORCESUB_CHANNEL_UNAME", "")
+BOTOWNER_UNAME = environ.get("BOTOWNER_UNAME", "")
+
+
