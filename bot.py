2e2717fea17bb1aaff89a9e469146af4034b4066
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-02-03 09:11:02 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+#Sᴜɴʀɪsᴇs Hᴀʀsʜᴀ 𝟸𝟺 🇮🇳 ᵀᴱᴸ
+import requests, wget
+from pyrogram import Client, filters
+from config import *
+from pyrogram.types import *
+
+
+#ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
+
+# Initialize the Pyrogram client
+app = Client(
+    "image_editor_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+print("Bot Started!🦋 © t.me/Sunrises_24")
+                           
+# Run the bot
+app.run()
